2a47aea21273e4319c00c5c4e29a0e17edbcc535
Jeff Bailey <northwestjeff@gmail.com>
2017-12-17 16:23:34 -0800

    First commit

---
+++ Averager/Averager/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Treehouse.Averager
+{
+
+    class Program
+    {
+
+        static void Main()
+        {
+            var running = true;
+            int timesEntered = 0;
+            int total = 0;
+
+
+            Console.WriteLine("Welcome to Averager.  Give me some numbers and I'll average them for you!");
+
+
+            while (running)
+            {
+                Console.WriteLine("Please enter a number or type a letter to compute an average");
+                try
+                {
+                    var input = Console.ReadLine();
+                    total += int.Parse(input);
+                    timesEntered += 1;
+                    Console.WriteLine("Running total: " + total);
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Total entered: " + total + ". Divided by " + timesEntered + ", averages " + (total / timesEntered));
+                    break;
+                }
+
+            }
+
+        }
+    }
+}
+
+
